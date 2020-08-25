4d5998f85d1540aac60fd5ad883bc549361760bd
BradRowley <69436623+BradRowley@users.noreply.github.com>
2020-08-25 09:43:21 -0400

    Update Find the smallest integer in the array.cs

--- Find the smallest integer in the array.cs
+++ Find the smallest integer in the array.cs
@@ -4,3 +4,9 @@ int[] array = { 10, 22, 8, 4 };
 min = array[i];
 
 ////NOT DONE////
+// take first int in the array store it in variable lp
+// take next int in the array and compare it the int in lp
+// if the int is less than the int lp, replace it 
+// do it until the end of the array r
+// eturn lp
+// Dan's PEDAC
